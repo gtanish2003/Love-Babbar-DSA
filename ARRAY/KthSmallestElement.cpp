7a65a03243493b1aeaabd9030700d3aeed02efec
Tanish Gupta <gtanish2003@gmail.com>
2023-10-24 23:08:29 +0530

    Initiated Kth smallest Element ques

---
+++ ARRAY/KthSmallestElement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+
+
+int main(){
+
+    int arr[100];
+    int n;
+    cout<<"Entger the value of n:"<<endl;
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+
+
+    return 0;
+}
